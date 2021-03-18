216e6fd98eca95fdb62496ffe61ac1854d8129c4
Victor Torres <victoorres@icloud.com>
2021-03-18 18:05:52 -0300

    Spring adicionado + Conexão com MySQL

---
+++ src/test/java/br/com/desafiobackend/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
